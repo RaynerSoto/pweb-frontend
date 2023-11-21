3221b6f82e3007003470843cc1959e7d745b34c0
Rayner Alejandro Soto Martínez <raynersoto01@gmail.com>
2023-11-21 16:08:46 -0500

    Añadiendo las Excepciones y su contructor

--- src/main/java/cu/edu/cujae/pweb/exception/BadException.java
+++ src/main/java/cu/edu/cujae/pweb/exception/BadException.java
@@ -1,4 +1,22 @@
 package cu.edu.cujae.pweb.exception;
 
 public class BadException extends Exception{
+    public BadException() {
+    }
+
+    public BadException(String message) {
+        super(message);
+    }
+
+    public BadException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public BadException(Throwable cause) {
+        super(cause);
+    }
+
+    public BadException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
--- src/main/java/cu/edu/cujae/pweb/exception/GoodException.java
+++ src/main/java/cu/edu/cujae/pweb/exception/GoodException.java
@@ -1,4 +1,22 @@
 package cu.edu.cujae.pweb.exception;
 
 public class GoodException extends Exception{
+    public GoodException() {
+    }
+
+    public GoodException(String message) {
+        super(message);
+    }
+
+    public GoodException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public GoodException(Throwable cause) {
+        super(cause);
+    }
+
+    public GoodException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
