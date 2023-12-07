547fb5576a9012ede7dac104a5b7c0bbeeb56d7e
Rayner Alejandro Soto Martínez <raynersoto01@gmail.com>
2023-12-07 12:28:13 -0500

    Prueba eliminar conseguida

--- src/main/java/cu/edu/cujae/pweb/util/ResponseEnvioUtil.java
+++ src/main/java/cu/edu/cujae/pweb/util/ResponseEnvioUtil.java
@@ -1,4 +1,17 @@
 package cu.edu.cujae.pweb.util;
 
 public class ResponseEnvioUtil {
+    private String message_envio;
+
+    public ResponseEnvioUtil(String message_envio) {
+        this.message_envio = message_envio;
+    }
+
+    public String getMessage_envio() {
+        return message_envio;
+    }
+
+    public void setMessage_envio(String message_envio) {
+        this.message_envio = message_envio;
+    }
 }
